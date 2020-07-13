fa1cdf2b1e8f163a2899c0bed9e40dc96e1b545b
Jake <jakehofmeester@hotmail.com>
2020-07-13 13:45:47 +0200

    Updated rank card and welcome message

--- src/bot/events/onGuildMemberAdd.js
+++ src/bot/events/onGuildMemberAdd.js
@@ -1,7 +1,7 @@
 module.exports = function (member) {
   member.guild.channels.cache
     .get("731870049173897288")
-    .send(`Hey ${member}, welcome to **GGBN's Server**! `);
+    .send(`Hey ${member}, welcome to **Team Equanimity's Server**! `);
 
   member.roles.add(global.gConfig.default_role);
 };
